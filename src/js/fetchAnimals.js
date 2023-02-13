1841c62cfec15093f8417f47b2e18fea927f355f
Igor <118106419+IgorBoichuk@users.noreply.github.com>
2023-02-13 18:36:39 +0200

    something is done

--- src/js/fetchAnimals.js
+++ src/js/fetchAnimals.js
@@ -1,19 +1,59 @@
-function fetchAnimals(query) {
-  const myApiKey = '33589434-498505a5cafca5b4759d2d286';
+import axios from 'axios';
 
-  return fetch(`https://pixabay.com/api/?key=${myApiKey}&q=${query}`)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    })
-    .then(data => {
-      console.log(data);
-    })
-    .catch(error => {
-      console.log(error);
-    });
+const myApiKey = '33589434-498505a5cafca5b4759d2d286';
+
+async function fetchAnimals(query, perPage, page) {
+  const url = `https://pixabay.com/api/?key=${myApiKey}&q=${query}&image_type=photo&pretty=true&per_page=${perPage}&page=${page}&orientation=horizontal&safesearch=true`;
+  try {
+    const { data } = await axios.get(url);
+
+    return data;
+  } catch (error) {}
 }
-
 export { fetchAnimals };
+
+// -----------------------------------------------------------------------------------------------------------------
+// function fetchAnimals(query) {
+//   const myApiKey = '33589434-498505a5cafca5b4759d2d286';
+
+//   return fetch(`https://pixabay.com/api/?key=${myApiKey}&q=${query}`)
+//     .then(response => {
+//       if (!response.ok) {
+//         throw new Error(response.status);
+//       }
+//       return response.json();
+//     })
+//     .then(data => {
+//       // data - это мои элементы - по ним создать карточки по свойствам объекта
+//       console.log(data);
+//       // прописать ф-цию создания ДОМ элемента через (МАР смотреть документацию библиотеки)
+//     })
+//     .catch(error => {
+//       console.log(error);
+//     });
+// }
+// export { fetchAnimals };
+// -----------------------------------------------------------------------------------------------------------------
+// const body = document.querySelector('body');
+
+// async function fetchAnimals(query) {
+//   const myApiKey = '33589434-498505a5cafca5b4759d2d286';
+//   const url = `https://pixabay.com/api/?key=${myApiKey}&q=${query}`;
+//   const response = await fetch(url);
+//   const data = await response.json();
+//   console.log(data);
+
+//   const html = data.hits
+//     .map(elem => {
+//       return `<img src='${elem.previewURL}'>`;
+//     })
+//     .join('');
+
+//   console.log(html);
+//   return html;
+
+// }
+
+// function appendHtml(html) {
+//   document.querySelector('.gallery').insertAdjacentHTML('afterbegin', html);
+// }
--- src/js/index.js
+++ src/js/index.js
@@ -5,15 +5,59 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 
 import { fetchAnimals } from './fetchAnimals';
 
+const gallery = document.querySelector('.gallery');
 const form = document.querySelector('.search-form');
 
-form.addEventListener('submit', event => {
+let perPage = 40;
+let page = 1;
+let maxPage = 0;
+let query = '';
+
+form.addEventListener('submit', async event => {
+  query = event.target.elements.searchQuery.value;
   event.preventDefault();
 
-  query = form.elements.searchQuery.value;
-  console.log(query);
+  inputFormValue = query.toLowerCase().trim();
 
-  fetchAnimals(query);
+  if (inputFormValue === '') {
+    return;
+  }
+  const data = await fetchAnimals(query, perPage, page);
+  console.log(data.hits);
+  markupContent(data);
 });
 
-// fetch is done, need make the DOM elements Creator
+function markupContent(data) {
+  const markup = data.hits
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<div class="photo-card">
+        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+        <div class="info">
+          <p class="info-item">
+            <b>Likes ${likes}</b>
+          </p>
+          <p class="info-item">
+            <b>Views ${views}</b>
+          </p>
+          <p class="info-item">
+            <b>Comments ${comments}</b>
+          </p>
+          <p class="info-item">
+            <b>Downloads ${downloads}</b>
+          </p>
+        </div>
+      </div>`;
+      }
+    )
+    .join('');
+  gallery.insertAdjacentHTML('afterbegin', markup);
+}
